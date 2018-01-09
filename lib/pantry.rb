f0a7e2178bf0b8542355a0db9d7607c2de184a39
mgmilton <matt.milton@gmail.com>
2018-01-09 09:05:27 -0700

    first commit

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,5 @@
 class Pantry
+  def initialize
+  end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,12 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/pantry'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 end
