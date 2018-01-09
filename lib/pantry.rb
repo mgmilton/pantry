99a7293d95bdb1b02629b497eb9df7b52b55cbbd
mgmilton <matt.milton@gmail.com>
2018-01-09 09:19:34 -0700

    through iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,5 +1,16 @@
 class Pantry
+  attr_reader :stock, :stock_check
+
   def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    @stock.fetch(item, 0)
+  end
+
+  def restock(item, quantity)
+    @stock[item] = quantity + stock_check(item)
   end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -9,4 +9,31 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
   end
+
+  def test_stock_returns_a_hash
+    pantry = Pantry.new
+    assert_instance_of Hash, pantry.stock
+  end
+
+  def test_stock_check_retuns_zero
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_restock_adds_items_to_stock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_restock_reassigns_items_to_stock
+    pantry = Pantry.new
+
+    pantry.restock("Cheese", 10)
+
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check("Cheese")
+  end
 end
