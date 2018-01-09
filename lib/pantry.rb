111ab803a7b2f7e43acf22d4ea2edbe3e92dd314
mgmilton <matt.milton@gmail.com>
2018-01-09 09:25:57 -0700

    updates pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,5 +1,7 @@
+require './lib/recipe'
+
 class Pantry
-  attr_reader :stock, :stock_check
+  attr_reader :stock
 
   def initialize
     @stock = {}
@@ -11,6 +13,14 @@ class Pantry
 
   def restock(item, quantity)
     @stock[item] = quantity + stock_check(item)
+  end
+
+  def shopping_list(recipe)
+    recipe.ingredients
+  end
+
+  def add_to_shopping_list(recipe)
+
   end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -36,4 +36,12 @@ class PantryTest < Minitest::Test
 
     assert_equal 30, pantry.stock_check("Cheese")
   end
+
+  def test_shopping_list_returns_ingredients
+    recipe = Recipe.new("Cheese Pizza")
+    recipe.add_ingredient("Cheese", 20)
+    recipe.add_ingredient("Flour", 20)
+    pantry = Pantry.new
+    assert_equal {"Cheese"=>20, "Flour"=>20}, pantry.shopping_list(recipe)
+  end
 end
